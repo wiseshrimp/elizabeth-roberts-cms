e40c0f0b0fcd882b02d2be08998935e14a5a5152
Sue Roh <sue_roh@newyorker.com>
2026-01-24 21:10:00 -0500

    added jobs

--- schemas/documents/job.ts
+++ schemas/documents/job.ts
@@ -1,19 +1,31 @@
 import slug from '../reusables/slug'
 
 const jobPage = {
-    type: 'document',
-    name: 'jobPage',
-    fields: [
-        {
-            name: 'title',
-            type: 'string'
-        },
-        slug,
-        {
-            name: 'description',
-            type: 'textEditor'
-        }
-    ]
+  type: 'document',
+  name: 'jobPage',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+    },
+    slug,
+    {
+      name: 'description',
+      type: 'textEditor',
+    },
+    {
+      name: 'responsibilities',
+      type: 'textEditor',
+    },
+    {
+      name: 'requirements',
+      type: 'textEditor',
+    },
+    {
+      name: 'applicationInstructions',
+      type: 'textEditor',
+    },
+  ],
 }
 
 export default jobPage
