c7112ffdb0430adb52aed2cb7de511216fcaa27a
Sue Roh <sue_roh@newyorker.com>
2026-02-03 14:59:14 -0500

    fix: allow emails to be strings not links:

--- schemas/types/settings/navigation.ts
+++ schemas/types/settings/navigation.ts
@@ -1,100 +1,95 @@
 const reusable = {
-    type: 'object',
-    name: 'navigationItem',
-    fields: [
-        {
-            type: 'string',
-            name: 'title'
-        },
-        {
-            type: 'string',
-            name: 'link'
-        },
-        {
-            type: 'string',
-            name: 'showFeatured',
-            title: 'Show Featured of Type',
-            description: 'Selecting an option will show the featured projects or objects in the navigation bar when hovering over the navigation item',
-            options: {
-                layout: "dropdown", 
-                list: [
-                    {title: 'None', value: 'none'},
-                    {title: 'Projects', value: 'projects'},
-                    {title: 'Objects', value: 'objects'}
-                ],
-            }
-        }
-    ]
+  type: 'object',
+  name: 'navigationItem',
+  fields: [
+    {
+      type: 'string',
+      name: 'title',
+    },
+    {
+      type: 'string',
+      name: 'link',
+    },
+    {
+      type: 'string',
+      name: 'showFeatured',
+      title: 'Show Featured of Type',
+      description:
+        'Selecting an option will show the featured projects or objects in the navigation bar when hovering over the navigation item',
+      options: {
+        layout: 'dropdown',
+        list: [
+          {title: 'None', value: 'none'},
+          {title: 'Projects', value: 'projects'},
+          {title: 'Objects', value: 'objects'},
+        ],
+      },
+    },
+  ],
 }
 
 const navigation = {
-    name: 'navigation',
-    type: 'object',
-    title: 'Navigation',
-    preview: {
-        prepare() {
-            return (
-                {
-                    title: 'Navigation'
-                }
-            )
-        }
+  name: 'navigation',
+  type: 'object',
+  title: 'Navigation',
+  preview: {
+    prepare() {
+      return {
+        title: 'Navigation',
+      }
     },
-    fields: [
+  },
+  fields: [
+    {
+      type: 'object',
+      name: 'mainNavigation',
+      title: 'Main Navigation',
+      fields: [
         {
-            type: 'object',
-            name: 'mainNavigation',
+          name: 'navigationItems',
+          type: 'array',
+          of: [reusable],
+        },
+      ],
+      preview: {
+        prepare() {
+          return {
             title: 'Main Navigation',
-            fields: [
-                {
-                    name: 'navigationItems',
-                    type: 'array',
-                    of: [
-                        reusable
-                    ]
-                }
-            ],
-            preview: {
-                prepare() {
-                    return {
-                        title: 'Main Navigation'
-                    }
-                }
-            }
+          }
+        },
+      },
+    },
+    {
+      type: 'object',
+      name: 'footerNavigation',
+      fields: [
+        {
+          name: 'navigationItems',
+          type: 'array',
+          of: [reusable],
         },
         {
-            type: 'object',
-            name: 'footerNavigation',
-            fields: [
+          name: 'socials',
+          type: 'array',
+          of: [
+            {
+              type: 'object',
+              fields: [
                 {
-                    name: 'navigationItems',
-                    type: 'array',
-                    of: [
-                        reusable
-                    ]
+                  type: 'string',
+                  name: 'title',
                 },
                 {
-                    name: 'socials',
-                    type: 'array',
-                    of: [
-                        {
-                            type: 'object',
-                            fields: [
-                                {
-                                    type: 'string',
-                                    name: 'title'
-                                },
-                                {
-                                    type: 'url',
-                                    name: 'link'
-                                }
-                            ]
-                        }
-                    ]
+                  type: 'string',
+                  name: 'link',
                 },
-            ]
-        }
-    ]
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
 }
 
 export default navigation
